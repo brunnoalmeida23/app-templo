e2880d4657453a5bdca11d480deaf625dfd2a814
Bruno Almeida <brunno_almeida@hotmail.com.br>
2026-07-19 17:08:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -292,7 +292,7 @@ def cadastrar_publicacao():
         flash(f'✅ {tipo.capitalize()} cadastrado(a) com sucesso!')
         if tipo == 'aviso':
             enviar_notificacao("📢 Novo Aviso - TUPBAO", titulo)
-	if tipo == 'limpeza':
+        if tipo == 'limpeza':
             enviar_notificacao("🧹 Nova Limpeza - TUPBAO", titulo)
         return redirect(url_for('admin'))
     funcao = session.get('funcao', 'membro')
